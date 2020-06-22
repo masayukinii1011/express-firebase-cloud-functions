bb007055ff7ef3b85c62f327c9d05ede5342fbaf
masayukinii1011 <masayukinii1011@gmail.com>
2020-06-22 14:35:58 +0900

    秘密鍵登録してdbにアクセス

--- functions/routers/messages/route.js
+++ functions/routers/messages/route.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const admin = require('firebase-admin')
+
+const serviceAccount = require("../../serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://nii-express-cloud-functions.firebaseio.com"
+});
+
 const router = express.Router()
 const endPoint = '/messages'
+const db = admin.firestore()
 
 router.route(endPoint)
   .get((req, res) => {
-
+    const messages = []
+    try {
+      const querySnapshot = await db.collection('messages').get()
+      querySnapshot.forEach(doc => {
+        messages.push({
+          id: doc.id,
+          ...doc.data()
+        })
+      })
+    } catch (error) {
+      console.error(error)
+    }
+    res.json({ messages })
   })
   .post((req, res) => {
 
